07c2edd935c9bb6e4dc29216aaeca0e8ef90fd62
Picalines <picalines0@gmail.com>
2023-10-10 15:26:00 +0300

    separate textobjects config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,10 +5,22 @@ return {
 	},
 	build = ':TSUpdate',
 	config = function()
-		require('nvim-treesitter.configs').setup {
+		require('nvim-treesitter.configs').setup({
 			-- Add languages to be installed here that you want installed for treesitter
-			ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript',
-				'typescript', 'vimdoc', 'vim' },
+			ensure_installed = {
+				'c',
+				'cpp',
+				'go',
+				'lua',
+				'python',
+				'rust',
+				'tsx',
+				'javascript',
+				'typescript',
+				'java',
+				'vimdoc',
+				'vim',
+			},
 
 			-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
 			auto_install = false,
@@ -18,57 +30,18 @@ return {
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = '<c-space>',
-					node_incremental = '<c-space>',
-					scope_incremental = '<c-s>',
-					node_decremental = '<M-space>',
+					init_selection = '<C-s>',
+					node_incremental = '<C-s>',
+					scope_incremental = '<C-s>',
+					node_decremental = '<M-s>',
 				},
 			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						['aa'] = '@parameter.outer',
-						['ia'] = '@parameter.inner',
-						['af'] = '@function.outer',
-						['if'] = '@function.inner',
-						['ac'] = '@class.outer',
-						['ic'] = '@class.inner',
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_start = {
-						[']m'] = '@function.outer',
-						[']]'] = '@class.outer',
-					},
-					goto_next_end = {
-						[']M'] = '@function.outer',
-						[']['] = '@class.outer',
-					},
-					goto_previous_start = {
-						['[m'] = '@function.outer',
-						['[['] = '@class.outer',
-					},
-					goto_previous_end = {
-						['[M'] = '@function.outer',
-						['[]'] = '@class.outer',
-					},
-				},
-				swap = {
-					enable = true,
-					swap_next = {
-						['<leader>a'] = '@parameter.inner',
-					},
-					swap_previous = {
-						['<leader>A'] = '@parameter.inner',
-					},
-				},
-			},
-		}
+
+			-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+			-- context_commentstring = {
+			-- 	enable = true,
+			-- 	enable_autocmd = false,
+			-- },
+		})
 	end
 }
-
