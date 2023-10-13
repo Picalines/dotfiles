8d1208ce57830866664b4627ee4af871a8a6096e
Picalines <picalines0@gmail.com>
2023-10-14 00:14:26 +0300

    nvim-tree git icons & hjkl maps

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,28 +6,63 @@ return {
 		'nvim-tree/nvim-web-devicons',
 	},
 	config = function()
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
+		local nvim_tree = require('nvim-tree')
+
+		vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>')
 
 		local function on_attach(bufnr)
 			local api = require('nvim-tree.api')
+
+			local function map_key(key, fn, opts)
+				return vim.keymap.set('n', key, fn, opts)
+			end
 
 			local function opts(desc)
 				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 			end
 
-			-- default mappings
 			api.config.mappings.default_on_attach(bufnr)
 
-			-- custom mappings
-			vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
-			vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+			map_key('<C-P>', api.tree.change_root_to_parent, opts('Up'))
+			map_key('?', api.tree.toggle_help, opts('Help'))
+
+			map_key('h', api.tree.close, opts('Close'))
+			map_key('l', api.node.open.edit, opts('Edit Or Open'))
+			map_key('H', api.tree.collapse_all, opts('Collapse All'))
 		end
 
-		require('nvim-tree').setup({
-			on_attach = on_attach
-		})
+		nvim_tree.setup({
+			on_attach = on_attach,
 
-		vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+			disable_netrw = true,
+			hijack_netrw = true,
+			hijack_cursor = true,
+
+			diagnostics = {
+				enable = true,
+			},
+
+			git = {
+				enable = true,
+				ignore = true,
+				timeout = 500,
+			},
+
+			renderer = {
+				icons = {
+					glyphs = {
+						git = {
+							unstaged = 'M',
+							staged = 'S',
+							unmerged = 'C',
+							renamed = 'R',
+							untracked = 'U',
+							deleted = 'D',
+							ignored = '-',
+						}
+					}
+				},
+			},
+		})
 	end,
 }
