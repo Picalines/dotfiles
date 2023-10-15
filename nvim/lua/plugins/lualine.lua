02aa05c1458dcec48a4730d324b41f329ec630e9
Picalines <picalines0@gmail.com>
2023-10-15 23:28:58 +0300

    lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,7 +6,19 @@ return {
 			theme = 'onedark',
 			component_separators = '|',
 			section_separators = '',
+
+			ignore_focus = { 'NvimTree' },
+
+			globalstatus = true,
+		},
+
+		sections = {
+			lualine_a = { 'mode' },
+			lualine_b = { 'branch', 'diff', 'diagnostics' },
+			lualine_c = { 'filename' },
+			lualine_x = { 'location', 'encoding' },
+			lualine_y = { 'filetype' },
+			lualine_z = {}
 		},
 	},
 }
-
