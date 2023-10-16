6f47b67abeb1cb7da6c2fcbcdcd6642f4e3529e3
Picalines <picalines0@gmail.com>
2023-10-16 16:34:01 +0300

    notify

---
+++ nvim/lua/plugins/notify.lua
@@ -0,0 +1,17 @@
+return {
+	'rcarriga/nvim-notify',
+	lazy = false,
+
+	config = function()
+		local notify = require('notify')
+
+		notify.setup({
+			render = "wrapped-compact",
+			stages = "slide",
+			timeout = 5000,
+			top_down = true,
+		})
+
+		vim.notify = notify
+	end
+}
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -8,25 +8,30 @@ return {
 		local nvim_tree = require('nvim-tree')
 		local api = require('nvim-tree.api')
 
-		vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>')
+		vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>', {
+			silent = true,
+			desc = 'Open File [E]xplorer',
+		})
 
 		local function on_attach(bufnr)
-			local function map_key(key, fn, opts)
-				return vim.keymap.set('n', key, fn, opts)
-			end
-
-			local function opts(desc)
-				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			local function map_key(key, fn, desc)
+				return vim.keymap.set('n', key, fn, {
+					desc = "nvim-tree: " .. desc,
+					buffer = bufnr,
+					noremap = true,
+					silent = true,
+					nowait = true,
+				})
 			end
 
 			api.config.mappings.default_on_attach(bufnr)
 
-			map_key('<C-P>', api.tree.change_root_to_parent, opts('Up'))
-			map_key('?', api.tree.toggle_help, opts('Help'))
+			map_key('<C-P>', api.tree.change_root_to_parent, 'Up')
+			map_key('?', api.tree.toggle_help, 'Help')
 
-			map_key('h', api.tree.close, opts('Close'))
-			map_key('l', api.node.open.edit, opts('Edit Or Open'))
-			map_key('H', api.tree.collapse_all, opts('Collapse All'))
+			map_key('h', api.tree.close, 'Close')
+			map_key('l', api.node.open.edit, 'Edit Or Open')
+			map_key('H', api.tree.collapse_all, 'Collapse All')
 		end
 
 		nvim_tree.setup({
