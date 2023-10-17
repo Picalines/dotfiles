825f71bd31e523b3649857cfd992e515a25d53c5
Picalines <picalines0@gmail.com>
2023-10-18 00:13:37 +0300

    nvim-cmp icons

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,3 +1,31 @@
+local kind_icons = {
+	Text = '',
+	Method = '󰆧',
+	Function = '󰊕',
+	Constructor = '',
+	Field = '',
+	Variable = '',
+	Class = '󰠱',
+	Interface = '',
+	Module = '',
+	Property = '',
+	Unit = '',
+	Value = '󰎠',
+	Enum = '',
+	Keyword = '󰌋',
+	Snippet = '',
+	Color = '󰏘',
+	File = '󰈙',
+	Reference = '',
+	Folder = '󰉋',
+	EnumMember = '',
+	Constant = '',
+	Struct = '',
+	Event = '',
+	Operator = '󰆕',
+	TypeParameter = '',
+}
+
 return {
 	-- Autocompletion
 	'hrsh7th/nvim-cmp',
@@ -38,26 +66,13 @@ return {
 					behavior = cmp.ConfirmBehavior.Replace,
 					select = true,
 				}),
+			},
 
-				['<Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
-
-				['<S-Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
+			formatting = {
+				format = function(_, item)
+					item.kind = kind_icons[item.kind] .. ' ' .. item.kind:lower()
+					return item
+				end
 			},
 
 			sources = {
