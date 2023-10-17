a8e734603e3995c88adaa87c2570113380798e04
Picalines <picalines0@gmail.com>
2023-10-17 22:52:10 +0300

    sonokai white folders

--- nvim/lua/plugins/colorschemes/sonokai.lua
+++ nvim/lua/plugins/colorschemes/sonokai.lua
@@ -8,5 +8,16 @@ return {
 			let g:sonokai_enable_italic = 1
 			let g:sonokai_diagnostic_virtual_text = 'colored'
 		]]
+
+		vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+			pattern = "*",
+			command = [[
+				if g:colors_name ==# "sonokai"
+					highlight link NvimTreeFolderName Fg
+					highlight link NvimTreeEmptyFolderName Fg
+					highlight link NvimTreeOpenedFolderName Fg
+				endif
+			]]
+		})
 	end
 }
