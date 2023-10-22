1bc9290e4f257e3f5780bdd7704b894de2c6c049
Picalines <picalines0@gmail.com>
2023-10-22 23:28:14 +0300

    mason round border

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,8 +2,7 @@ return {
 	-- LSP
 	'neovim/nvim-lspconfig',
 	dependencies = {
-		-- Automatically install LSPs to stdpath for neovim
-		{ 'williamboman/mason.nvim', config = true },
+		'williamboman/mason.nvim',
 		'williamboman/mason-lspconfig.nvim',
 
 		'j-hui/fidget.nvim',
