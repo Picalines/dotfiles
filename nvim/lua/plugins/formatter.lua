1052e89eb2ba5a7e55bd03bf62352f663458cc26
Picalines <picalines0@gmail.com>
2023-10-24 13:06:24 +0300

    formatter save if was saved

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -23,33 +23,40 @@ return {
 
 		for _, filetype in ipairs(prettier_filetypes) do
 			local module = require('formatter.filetypes.' .. filetype)
-			manual_filetype_formatters[filetype] = module.prettierd or module.prettier
+			manual_filetype_formatters[filetype] = module.prettierd
 		end
 
+		local function builtin_or_lsp_formatter()
+			local filetype = vim.bo.filetype
+			if manual_filetype_formatters[filetype] == nil then
+				return
+			end
+
+			local has_builtin_formatter, builtin_module = pcall(require, 'formatter.filetypes.' .. filetype)
+			if has_builtin_formatter then
+				local _, builtin_formatter = next(builtin_module)
+				builtin_formatter() -- fallback to formatter.nvim's formatter
+			else
+				lsp_formatter() -- fallback to LSP's formatter
+			end
+		end
+
+		local formatters = vim.tbl_extend('error', manual_filetype_formatters, {
+			['*'] = {
+				require('formatter.filetypes.any').remove_trailing_whitespace,
+
+				builtin_or_lsp_formatter,
+
+				function()
+					if not vim.bo.modified then
+						vim.cmd.w()
+					end
+				end,
+			},
+		})
+
 		require('formatter').setup {
-			filetype = vim.tbl_extend('error', manual_filetype_formatters, {
-				['*'] = {
-					require('formatter.filetypes.any').remove_trailing_whitespace,
-
-					function()
-						local filetype = vim.bo.filetype
-						if manual_filetype_formatters[filetype] ~= nil then
-							return
-						end
-
-						local plugin_formatter_ok, builtin_module = pcall(require, 'formatter.filetypes.' .. filetype)
-						if plugin_formatter_ok then
-							local _, builtin_formatter = next(builtin_module)
-
-							-- fallback to formatter.nvim's formatter
-							builtin_formatter()
-						else
-							-- fallback to LSP's formatter
-							lsp_formatter()
-						end
-					end,
-				},
-			}),
+			filetype = formatters,
 		}
 	end,
 }
