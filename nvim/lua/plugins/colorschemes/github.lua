4088140ed6d520993ea9dfa6faa0326d2ded5056
Picalines <picalines0@gmail.com>
2023-10-25 23:48:07 +0300

    github colortheme

---
+++ nvim/lua/plugins/colorschemes/github.lua
@@ -0,0 +1,22 @@
+return {
+	'projekt0n/github-nvim-theme',
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require('github-theme').setup {
+			options = {
+				transparent = true,
+				styles = {
+					comments = 'italic',
+					keywords = 'bold',
+				},
+			},
+
+			groups = {
+				all = {
+					VertSplit = { link = 'WinBar' },
+				},
+			},
+		}
+	end,
+}
