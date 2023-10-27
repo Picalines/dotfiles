0c20644e9a23d7a0ff658046e6bdb0f2c932f5a1
Picalines <picalines0@gmail.com>
2023-10-27 17:44:40 +0300

    empty line

--- nvim/lua/plugins/indentguide.lua
+++ nvim/lua/plugins/indentguide.lua
@@ -6,6 +6,7 @@ return {
 	priority = 1001,
 	config = function()
 		local hooks = require 'ibl.hooks'
+
 		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
 			vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#454545' })
 		end)
