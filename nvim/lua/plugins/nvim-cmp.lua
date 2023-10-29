7ab38bb3efb6486626d540c7e5bc13f786349a7d
Picalines <picalines0@gmail.com>
2023-10-29 21:23:36 +0300

    luasnip config

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -29,12 +29,40 @@ return {
 
 		luasnip.config.setup {}
 
+		local function has_words_before()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+		end
+
+		local function select_next(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end
+
+		local function select_prev(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end
+
 		cmp.setup {
 			mapping = cmp.mapping.preset.insert {
 				['<S-Space>'] = cmp.mapping.complete(),
 
-				['<Tab>'] = cmp.mapping.select_next_item(),
-				['<S-Tab>'] = cmp.mapping.select_prev_item(),
+				['<Tab>'] = cmp.mapping(select_next, { 'i', 's' }),
+				['<S-Tab>'] = cmp.mapping(select_prev, { 'i', 's' }),
 
 				['<C-k>'] = cmp.mapping.scroll_docs(-4),
 				['<C-j>'] = cmp.mapping.scroll_docs(4),
