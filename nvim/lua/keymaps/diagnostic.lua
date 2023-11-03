43e395949f3830a4486d335578bae0370413bb03
Picalines <picalines0@gmail.com>
2023-11-03 21:24:25 +0300

    diagnostic move maps

--- nvim/lua/keymaps/diagnostic.lua
+++ nvim/lua/keymaps/diagnostic.lua
@@ -2,7 +2,7 @@ local function map_key(key, func, desc)
 	return vim.keymap.set('n', key, func, { desc = desc })
 end
 
-map_key('[D', vim.diagnostic.goto_prev, 'Go to previous [d]iagnostic')
-map_key(']D', vim.diagnostic.goto_next, 'Go to next [d]iagnostic')
+map_key('[d', vim.diagnostic.goto_prev, 'Go to previous [d]iagnostic')
+map_key(']d', vim.diagnostic.goto_next, 'Go to next [d]iagnostic')
 map_key('<leader>Dm', vim.diagnostic.open_float, 'Open [d]iagnostic [m]essage')
 map_key('<leader>Dl', vim.diagnostic.setloclist, 'Open [d]iagnostics [l]ist')
