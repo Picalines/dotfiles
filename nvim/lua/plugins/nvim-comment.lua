b99d2f03584651df95a06e82924a24c0f9d47ca9
Picalines <picalines0@gmail.com>
2023-11-05 15:29:06 +0300

    comment mapping

--- nvim/lua/plugins/nvim-comment.lua
+++ nvim/lua/plugins/nvim-comment.lua
@@ -9,6 +9,10 @@ return {
 
 	config = function()
 		require('nvim_comment').setup {
+			line_mapping = '<leader>//',
+			operator_mapping = '<leader>/',
+			comment_chunk_text_object = 'ic',
+
 			hook = require('ts_context_commentstring.internal').update_commentstring,
 		}
 	end,
