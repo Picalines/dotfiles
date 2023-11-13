702c7b82b0253db30515f757caba7544b350be10
Picalines <picalines0@gmail.com>
2023-11-13 23:55:22 +0300

    g 0$ motions

--- nvim/lua/keymaps/motion.lua
+++ nvim/lua/keymaps/motion.lua
@@ -1,11 +1,15 @@
 local util = require 'keymaps.util'
 
 util.declare_keymaps {
-	n = {
+	[{ 'n', 'v' }] = {
 		['<C-u>'] = '<C-u>zz',
 		['<C-d>'] = '<C-d>zz',
 
 		['n'] = 'nzzzv',
 		['N'] = 'Nzzzv',
+
+		['gh'] = '^',
+		['gH'] = '0',
+		['gl'] = '$',
 	},
 }
