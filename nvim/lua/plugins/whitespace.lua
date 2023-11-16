c4cb2cf3b12f6d99c6e31c7a7c6037330435c354
Picalines <picalines0@gmail.com>
2023-11-16 14:56:23 +0300

    trailing whitespace

---
+++ nvim/lua/plugins/whitespace.lua
@@ -0,0 +1,9 @@
+return {
+	'johnfrankmorgan/whitespace.nvim',
+	opts = {
+		highlight = 'DiffDelete',
+
+		ignore_terminal = true,
+		ignored_filetypes = { 'help', 'noice', 'TelescopePrompt', 'neo-tree', 'toggleterm' },
+	},
+}
