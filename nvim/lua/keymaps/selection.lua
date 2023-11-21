5208a2f5861e51864f809f21930dfa8c8c479d88
Picalines <picalines0@gmail.com>
2023-11-21 14:31:43 +0300

    nvim-gomove

--- nvim/lua/keymaps/selection.lua
+++ nvim/lua/keymaps/selection.lua
@@ -1,11 +1,6 @@
-local util = require 'keymaps.util'
-
-util.declare_keymaps {
+require('keymaps.util').declare_keymaps {
 	opts = { silent = true },
 	v = {
-		['J'] = ":m '>+1<CR>gv=gv",
-		['K'] = ":m '<-2<CR>gv=gv",
-
 		-- keep clipboard after paste in visual mode
 		['p'] = '"_dP',
 	},
---
+++ nvim/lua/plugins/nvim-gomove.lua
@@ -0,0 +1,13 @@
+return {
+	'booperlv/nvim-gomove',
+
+	opts = {
+		-- <A-hjkl> move
+		-- <A-HJKL> duplicate
+		map_defaults = true,
+
+		reindent = true,
+		undojoin = true,
+		move_past_end_col = false,
+	},
+}
