e2fb2314cdceccb0c0948766ddd88a6955b01f7e
Picalines <picalines0@gmail.com>
2023-12-11 22:38:07 +0300

    neovide remove blur & shadow

--- nvim/lua/settings/neovide.lua
+++ nvim/lua/settings/neovide.lua
@@ -2,8 +2,6 @@ vim.g.neovide_refresh_rate = 90
 
 vim.o.guifont = 'Hack Nerd Font Mono'
 
-vim.g.neovide_transparency = 0.8
-
 vim.g.neovide_cursor_animation_length = 0.05
 vim.g.neovide_scroll_animation_length = 0.1
 vim.g.neovide_position_animation_length = 0.1
