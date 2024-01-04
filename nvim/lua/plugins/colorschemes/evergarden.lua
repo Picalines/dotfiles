7c0ae137632a6b2e660f73b912960d0fcc246c9b
Picalines <picalines0@gmail.com>
2024-01-05 02:44:51 +0300

    evergarden

---
+++ nvim/lua/plugins/colorschemes/evergarden.lua
@@ -0,0 +1,25 @@
+return {
+	'crispybaccoon/evergarden',
+
+	lazy = false,
+
+	priority = 1000,
+
+	opts = {
+		transparent_background = false,
+
+		contrast_dark = 'hard',
+
+		override_terminal = true,
+
+		style = {
+			tabline = { reverse = true, color = 'green' },
+			search = { reverse = false, inc_reverse = true },
+			types = { italic = true },
+			keyword = { italic = true },
+			comment = { italic = true },
+		},
+
+		overrides = {},
+	},
+}
