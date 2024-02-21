90a8f226b299cb3f92078fa9a70cb335d911db18
Picalines <picalines0@gmail.com>
2024-02-21 23:47:12 +0300

    fullscreen neovide keymap

--- nvim/lua/keymaps/neovide/init.lua
+++ nvim/lua/keymaps/neovide/init.lua
@@ -4,9 +4,15 @@ local function zoom(delta)
 	vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
 
+local function toggle_fullscreen()
+	vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+end
+
 util.declare_keymaps {
 	n = {
 		['<C-=>'] = util.curry(zoom, 1.1),
 		['<C-->'] = util.curry(zoom, 1 / 1.1),
+
+		['<leader><C-f>'] = toggle_fullscreen,
 	},
 }
