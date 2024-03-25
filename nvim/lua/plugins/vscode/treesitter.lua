bd3aa126ce54fe791c24790c16eddbfd84f85885
Picalines <picalines0@gmail.com>
2024-03-25 23:25:02 +0300

    remove "(n)vim" from plugin filenames

--- nvim/lua/plugins/vscode/treesitter.lua
+++ nvim/lua/plugins/vscode/treesitter.lua
@@ -1,6 +1,6 @@
 local util = require 'util'
 
-local base = require 'plugins.nvim.nvim-treesitter'
+local base = require 'plugins.nvim.treesitter'
 
 return util.override_deep(base, {
 	config = function()
