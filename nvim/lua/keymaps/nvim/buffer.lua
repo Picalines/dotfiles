c7399f161dbc1444730dc903ef54102198980e6c
Picalines <picalines0@gmail.com>
2024-04-02 01:22:26 +0300

    fix: save keymap

--- nvim/lua/keymaps/nvim/buffer.lua
+++ nvim/lua/keymaps/nvim/buffer.lua
@@ -4,18 +4,6 @@ local function is_modified(buffer)
 	return vim.api.nvim_buf_get_option(buffer, 'modified')
 end
 
-local function save_buffer()
-	local filename = vim.api.nvim_buf_get_name(0)
-
-	if filename == '' then
-		filename = vim.fn.input { prompt = 'Save to: ' } or ''
-	end
-
-	if filename ~= '' then
-		vim.cmd(':silent! w ' .. filename)
-	end
-end
-
 local function remove_buffer()
 	local get_current_win = vim.api.nvim_get_current_win
 	local get_current_buf = vim.api.nvim_get_current_buf
@@ -24,7 +12,7 @@ local function remove_buffer()
 	local buf = get_current_buf()
 
 	if is_modified(0) then
-		save_buffer()
+		vim.cmd ':silent w'
 	end
 
 	vim.cmd ':bp'
@@ -41,7 +29,7 @@ util.declare_keymaps {
 		silent = true,
 	},
 	n = {
-		['<leader>s'] = { save_buffer, '[S]ave file' },
+		['<leader>s'] = { ':silent w<CR>', '[S]ave file' },
 
 		['<C-b>e'] = { ':enew<CR>', '[E]empty [b]uffer' },
 		['<C-b>c'] = { remove_buffer, '[C]lose [b]uffer' },
