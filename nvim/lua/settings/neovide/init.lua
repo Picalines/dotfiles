ca3c53dcf5efaacc7eca37abad37708c167e6944
Picalines <picalines0@gmail.com>
2024-04-14 15:44:01 +0300

    tweak neovide timing

--- nvim/lua/settings/neovide/init.lua
+++ nvim/lua/settings/neovide/init.lua
@@ -2,6 +2,8 @@ vim.g.neovide_refresh_rate = 90
 
 vim.o.guifont = 'Hack Nerd Font Mono'
 
-vim.g.neovide_cursor_animation_length = 0.05
+vim.g.neovide_scale_factor = 0.9
+
+vim.g.neovide_cursor_animation_length = 0.03
 vim.g.neovide_scroll_animation_length = 0.1
 vim.g.neovide_position_animation_length = 0.1
