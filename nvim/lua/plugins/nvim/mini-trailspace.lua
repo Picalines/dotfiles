b2556b5cabd1f0e5597b3f764f9f9361191d1c26
Picalines <picalines0@gmail.com>
2024-05-23 20:20:26 +0300

    fix: trailspace highlights as visual mode

--- nvim/lua/plugins/nvim/mini-trailspace.lua
+++ nvim/lua/plugins/nvim/mini-trailspace.lua
@@ -6,4 +6,10 @@ return {
 	opts = {
 		only_in_normal_buffers = true,
 	},
+
+	config = function(_, opts)
+		require('mini.trailspace').setup(opts)
+
+		vim.api.nvim_set_hl(0, 'MiniTrailspace', { link = 'Visual' })
+	end,
 }
