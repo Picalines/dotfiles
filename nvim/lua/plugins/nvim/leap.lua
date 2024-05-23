bbc48deeee1b0a9b9a44079003389d585dbedfc5
Picalines <picalines0@gmail.com>
2024-05-23 20:05:44 +0300

    leap backdrop

--- nvim/lua/plugins/nvim/leap.lua
+++ nvim/lua/plugins/nvim/leap.lua
@@ -1,18 +1,17 @@
 return {
 	'ggandor/leap.nvim',
 
-	event = 'VeryLazy',
-
 	dependencies = {
-		{
-			'ggandor/flit.nvim',
-			opts = {},
-		},
+		'ggandor/flit.nvim',
 	},
 
 	config = function()
 		local leap = require 'leap'
 
+		leap.opts.highlight_unlabeled_phase_one_targets = true
+
 		leap.add_default_mappings()
+
+		vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
 	end,
 }
