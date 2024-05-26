4ba30f891f644d36c8af99d79804368b355a7bbe
Picalines <picalines0@gmail.com>
2024-05-26 16:40:26 +0300

    fix: autotag new configuration scheme

--- nvim/lua/plugins/nvim/treesitter.lua
+++ nvim/lua/plugins/nvim/treesitter.lua
@@ -1,58 +1,71 @@
+---@diagnostic disable: missing-fields
+
 return {
 	'nvim-treesitter/nvim-treesitter',
 
 	event = { 'BufReadPre', 'BufNewFile' },
 
-	dependencies = {
-		'nvim-treesitter/nvim-treesitter-textobjects',
-		'windwp/nvim-ts-autotag',
-	},
-
 	build = ':TSUpdate',
 
-	config = function()
-		require('nvim-treesitter.configs').setup {
-			ensure_installed = {
-				'c',
-				'cpp',
-				'go',
-				'lua',
-				'python',
-				'rust',
-				'javascript',
-				'typescript',
-				'tsx',
-				'html',
-				'css',
-				'json',
-				'toml',
-				'yaml',
-				'http',
-				'java',
-				'c_sharp',
-				'dockerfile',
-				'gitignore',
-				'bash',
-				'markdown',
-				'vimdoc',
-				'vim',
+	opts = {
+		ensure_installed = {
+			'c',
+			'cpp',
+			'go',
+			'lua',
+			'python',
+			'rust',
+			'javascript',
+			'typescript',
+			'tsx',
+			'html',
+			'css',
+			'json',
+			'toml',
+			'yaml',
+			'http',
+			'java',
+			'c_sharp',
+			'dockerfile',
+			'gitignore',
+			'bash',
+			'markdown',
+			'vimdoc',
+			'vim',
+		},
+
+		auto_install = false,
+
+		highlight = { enable = true },
+
+		indent = { enable = true },
+
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = 'vn',
+				node_incremental = 'n',
+				scope_incremental = false,
+				node_decremental = 'N',
 			},
+		},
+	},
 
-			auto_install = false,
-
-			highlight = { enable = true },
-			indent = { enable = true },
-			autotag = { enable = true },
-
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = 'vn',
-					node_incremental = 'n',
-					scope_incremental = false,
-					node_decremental = 'N',
-				},
-			},
-		}
+	config = function(_, opts)
+		require('nvim-treesitter.configs').setup(opts)
 	end,
+
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter-textobjects',
+
+		{
+			'windwp/nvim-ts-autotag',
+
+			opts = {
+				enable_close = true,
+				enable_rename = true,
+				enable_close_on_slash = true,
+			},
+		},
+	},
 }
