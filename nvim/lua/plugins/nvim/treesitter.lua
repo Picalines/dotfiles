a58db3b8ac74988249abb9b2da8d02872f059149
picalines <picalines0@gmail.com>
2024-06-13 09:21:51 +0300

    fix: new ts-autotag opts

--- nvim/lua/plugins/nvim/treesitter.lua
+++ nvim/lua/plugins/nvim/treesitter.lua
@@ -62,9 +62,11 @@ return {
 			'windwp/nvim-ts-autotag',
 
 			opts = {
-				enable_close = true,
-				enable_rename = true,
-				enable_close_on_slash = true,
+				opts = {
+					enable_close = true,
+					enable_rename = true,
+					enable_close_on_slash = true,
+				},
 			},
 		},
 	},
