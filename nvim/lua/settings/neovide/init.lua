da07ab27f57527a1649467b9a27cc50137fa45aa
picalines <picalines0@gmail.com>
2024-06-23 00:28:34 +0300

    feat: update neovide

--- nvim/lua/settings/neovide/init.lua
+++ nvim/lua/settings/neovide/init.lua
@@ -2,6 +2,10 @@ local persist = require 'persist'
 
 vim.g.neovide_refresh_rate = 90
 
+vim.g.neovide_show_border = true
+
+vim.g.neovide_hide_mouse_when_typing = true
+
 vim.o.guifont = 'Hack Nerd Font Mono'
 
 vim.g.neovide_scale_factor = persist.get_item('neovide_scale_factor', 0.9)
@@ -9,3 +13,5 @@ vim.g.neovide_scale_factor = persist.get_item('neovide_scale_factor', 0.9)
 vim.g.neovide_cursor_animation_length = 0.03
 vim.g.neovide_scroll_animation_length = 0.1
 vim.g.neovide_position_animation_length = 0.1
+
+vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
