27d8ffc1cc87c55f34f7dec37af477ee91571654
picalines <picalines0@gmail.com>
2024-06-23 01:14:49 +0300

    feat: whichkey keymap

--- nvim/lua/plugins/nvim/which-key.lua
+++ nvim/lua/plugins/nvim/which-key.lua
@@ -2,4 +2,16 @@ return {
 	'folke/which-key.nvim',
 
 	opts = {},
+
+	config = function(_, opts)
+		local util = require 'util'
+
+		require('which-key').setup(opts)
+
+		util.declare_keymaps {
+			n = {
+				['<C-w>?'] = { ':WhichKey<CR>', 'Keymap help' },
+			},
+		}
+	end,
 }
