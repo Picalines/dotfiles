6e24bbe7744c1218668541968ec5b16b6ddea7cd
picalines <picalines0@gmail.com>
2024-06-26 20:28:26 +0300

    feat: stop lsp after <C-b>C

--- nvim/lua/keymaps/nvim/buffer.lua
+++ nvim/lua/keymaps/nvim/buffer.lua
@@ -25,9 +25,12 @@ local function close_buffer()
 end
 
 local function close_all_buffers()
-	vim.cmd ':silent wa!'
-	vim.cmd ':silent! %bd'
-	vim.cmd ':Neotree filesystem current reveal_force_cwd'
+	vim.cmd [[
+		:silent wa!
+		:silent! %bd
+		:Neotree filesystem current reveal_force_cwd
+		:LspStop
+	]]
 end
 
 util.declare_keymaps {
