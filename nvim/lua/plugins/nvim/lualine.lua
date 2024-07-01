d8321d109010c6c1872c8b9d18c5138650e6c749
picalines <picalines0@gmail.com>
2024-07-01 22:04:57 +0300

    feat: sane lualine config

--- nvim/lua/plugins/nvim/lualine.lua
+++ nvim/lua/plugins/nvim/lualine.lua
@@ -3,41 +3,40 @@ return {
 
 	config = function()
 		local util = require 'util'
+		local lualine = require 'lualine'
 
-		local noice_status = require('noice').api.status
-
-		local function get_neotree_winid()
-			local fs_state = require('neo-tree.sources.manager').get_state 'filesystem'
-			if not fs_state then
-				return nil
-			end
-
-			local is_opened = require('neo-tree.ui.renderer').is_window_valid(fs_state.winid)
-			return is_opened and fs_state.winid or nil
-		end
-
-		local function get_neotree_width()
-			local fs_winid = get_neotree_winid()
-			return fs_winid ~= nil and vim.api.nvim_win_get_width(fs_winid) or 0
-		end
-
-		local neotree_shift = {
-			function()
-				return string.rep(' ', math.max(0, get_neotree_width() - 1)) .. '│'
-			end,
-			cond = function()
-				return get_neotree_winid() ~= nil
-			end,
-			color = function()
-				return { fg = require('cokeline.hlgroups').get_hl_attr('WinSeparator', 'fg'), bg = 'NONE' }
-			end,
+		local lualine_sections = { 'a', 'b', 'c', 'x', 'y', 'z' }
+		local lualine_modes = {
+			'normal',
+			'insert',
+			'visual',
+			'replace',
+			'command',
+			'inactive',
 		}
 
-		local opts = {
+		local lualine_mode_theme = util.flat_map(lualine_sections, function(section)
+			return { [section] = { bg = 'none' } }
+		end)
+
+		local lualine_theme = util.flat_map(lualine_modes, function(mode)
+			return { [mode] = lualine_mode_theme }
+		end)
+
+		local function macro_component()
+			local recording_register = vim.fn.reg_recording()
+			if recording_register == '' then
+				return ''
+			else
+				return 'macro @' .. recording_register
+			end
+		end
+
+		lualine.setup {
 			options = {
 				icons_enabled = true,
-				theme = 'auto',
-				component_separators = '',
+				theme = lualine_theme,
+				component_separators = '·',
 				section_separators = '',
 
 				ignore_focus = { 'neo-tree' },
@@ -55,16 +54,17 @@ return {
 				lualine_b = {},
 
 				lualine_c = {
+					{
+						'mode',
+						color = { gui = 'bold' },
+					},
+					{
+						'macro',
+						fmt = macro_component,
+					},
 					'branch',
 					'diff',
 					'diagnostics',
-					{
-						---@diagnostic disable-next-line: undefined-field
-						noice_status.mode.get,
-						---@diagnostic disable-next-line: undefined-field
-						cond = noice_status.mode.has,
-						color = { fg = '#ff9e64' },
-					},
 				},
 
 				lualine_x = { 'location', 'encoding' },
@@ -75,54 +75,19 @@ return {
 			},
 		}
 
-		local SIDEBAR_SHIFT_ENABLED = false
-		if SIDEBAR_SHIFT_ENABLED then
-			table.insert(opts.sections.lualine_c, 0, neotree_shift)
+		local function refresh()
+			lualine.refresh { place = { 'statusline' } }
 		end
 
-		for _, section in pairs(opts.sections) do
-			for i, component in pairs(section) do
-				if type(component) ~= 'table' then
-					---@diagnostic disable-next-line: cast-local-type
-					component = { component }
-				end
-
-				---@diagnostic disable-next-line: assign-type-mismatch
-				section[i] = util.override_deep({ color = { bg = 'NONE' } }, component)
-			end
-		end
-
-		local modes = {
-			'normal',
-			'insert',
-			'visual',
-			'replace',
-			'command',
-			'inactive',
-		}
-
-		local remove_bg_cmd = table.concat(
-			util.map(modes, function(mode)
-				return 'highlight! lualine_c_' .. mode .. ' guibg=NONE'
-			end),
-			'\n'
-		)
-
-		vim.cmd(remove_bg_cmd)
-
-		vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
-			pattern = '*',
-			command = remove_bg_cmd,
+		vim.api.nvim_create_autocmd('RecordingEnter', {
+			callback = refresh,
 		})
 
-		local lualine = require 'lualine'
-
-		local orig_refresh = lualine.refresh
-		lualine.refresh = function(refresh_opts)
-			orig_refresh(refresh_opts)
-			vim.cmd(remove_bg_cmd)
-		end
-
-		lualine.setup(opts)
+		vim.api.nvim_create_autocmd('RecordingLeave', {
+			callback = function()
+				local timer = vim.loop.new_timer()
+				timer:start(50, 0, vim.schedule_wrap(refresh))
+			end,
+		})
 	end,
 }
