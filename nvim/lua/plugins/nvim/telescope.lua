34e5f6b4f74bbd633b5d25bfe11dfcf2d891f2de
picalines <picalines0@gmail.com>
2024-07-24 21:43:57 +0300

    fix(telescope): correct config for vertical layout

--- nvim/lua/plugins/nvim/telescope.lua
+++ nvim/lua/plugins/nvim/telescope.lua
@@ -47,7 +47,9 @@ return {
 				side_by_side = true,
 				layout_strategy = 'vertical',
 				layout_config = {
-					preview_height = 0.7,
+					vertical = {
+						preview_height = 0.7,
+					},
 				},
 			},
 
