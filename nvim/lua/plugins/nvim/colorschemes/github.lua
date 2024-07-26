b343a8224c4d03b282659935cbeace25093e2ade
picalines <picalines0@gmail.com>
2024-07-26 18:39:41 +0300

    feat(colorschemes): remove obsolete, add oldworld

--- nvim/lua/plugins/nvim/colorschemes/github.lua
+++ nvim/lua/plugins/nvim/colorschemes/github.lua
@@ -1,7 +1,10 @@
 return {
 	'projekt0n/github-nvim-theme',
+
 	lazy = false,
+
 	priority = 1000,
+
 	config = function()
 		require('github-theme').setup {
 			options = {
---
+++ nvim/lua/plugins/nvim/colorschemes/oldworld.lua
@@ -0,0 +1,18 @@
+return {
+	'dgox16/oldworld.nvim',
+
+	lazy = false,
+
+	priority = 1000,
+
+	config = function(_, opts)
+		require('oldworld').setup(opts)
+
+		vim.api.nvim_create_autocmd('ColorScheme', {
+			pattern = 'oldworld',
+			callback = function()
+				vim.api.nvim_set_hl(0, 'NormalNC', { link = 'Normal' })
+			end,
+		})
+	end,
+}
