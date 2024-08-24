c6127f620ce868c67dd9f5cf1d3f874fcc47fbb1
Picalines <picalines0@gmail.com>
2024-08-24 17:26:37 +0300

    feat(keymaps): use telescope actions instead of remaps

--- nvim/lua/keymaps/ui/colorscheme.lua
+++ nvim/lua/keymaps/ui/colorscheme.lua
@@ -3,43 +3,54 @@ local persist = require 'util.persist'
 
 local function open_colorscheme_picker()
 	local actions = require 'telescope.actions'
+	local actions_set = require 'telescope.actions.set'
 	local actions_state = require 'telescope.actions.state'
 	local pickers = require 'telescope.pickers'
 	local finders = require 'telescope.finders'
 	local sorters = require 'telescope.sorters'
 	local dropdown = require('telescope.themes').get_dropdown()
 
-	local function select_colorscheme(prompt_bufnr)
-		local colorscheme = actions_state.get_selected_entry()[1]
-		vim.cmd('colorscheme ' .. colorscheme)
-		persist.save_item('colorscheme', colorscheme)
-		actions.close(prompt_bufnr)
-	end
-
-	local function next_colorscheme(prompt_bufnr)
-		actions.move_selection_next(prompt_bufnr)
-		local selected = actions_state.get_selected_entry()
-		local cmd = 'colorscheme ' .. selected[1]
-		vim.cmd(cmd)
-	end
-
-	local function prev_colorscheme(prompt_bufnr)
-		actions.move_selection_previous(prompt_bufnr)
-		local selected = actions_state.get_selected_entry()
-		local cmd = 'colorscheme ' .. selected[1]
-		vim.cmd(cmd)
-	end
-
 	local colorschemes = vim.fn.getcompletion('', 'color')
+	local current_colorscheme = vim.g.colors_name or colorschemes[1]
+
+	local function update_colorscheme()
+		local current_entry = actions_state.get_selected_entry()
+		local colorscheme = current_entry[1]
+		vim.cmd.colorscheme(colorscheme)
+		return colorscheme
+	end
+
+	local function select_colorscheme()
+		local colorscheme = update_colorscheme()
+		persist.save_item('colorscheme', colorscheme)
+	end
+
+	local function reset_colorscheme()
+		vim.cmd.colorscheme(current_colorscheme)
+	end
 
 	local opts = {
+		prompt_title = 'Select colorscheme',
+
 		finder = finders.new_table(colorschemes),
 		sorter = sorters.get_generic_fuzzy_sorter {},
 
-		attach_mappings = function(_, map)
-			map('i', '<CR>', select_colorscheme)
-			map('i', '<C-j>', next_colorscheme)
-			map('i', '<C-k>', prev_colorscheme)
+		attach_mappings = function(prompt_bufnr)
+			---@diagnostic disable-next-line: undefined-field
+			actions.close:enhance {
+				post = reset_colorscheme,
+			}
+
+			---@diagnostic disable-next-line: undefined-field
+			actions_set.shift_selection:enhance {
+				post = update_colorscheme,
+			}
+
+			actions.select_default:replace(function()
+				actions.close(prompt_bufnr)
+				select_colorscheme()
+			end)
+
 			return true
 		end,
 	}
