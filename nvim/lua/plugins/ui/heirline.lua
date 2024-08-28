0873e5aa9f2a3020676f46b64f6bb929a3583374
picalines <picalines0@gmail.com>
2024-08-28 11:13:50 +0300

    fix(heirline): it's not symmetric, that's okay

--- nvim/lua/plugins/ui/heirline.lua
+++ nvim/lua/plugins/ui/heirline.lua
@@ -260,7 +260,7 @@ return {
 		local BufferLine = h_util.make_buflist(
 			Buffer,
 			{ provider = ' ', hl = { fg = 'win_separator' } },
-			{ provider = ' ', hl = { fg = 'win_separator' } },
+			{ provider = '', hl = { fg = 'win_separator' } },
 			function()
 				return buflist_cache
 			end,
