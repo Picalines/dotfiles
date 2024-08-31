9a89af390a9dcd03e82eceeb2c97064d68db6d08
picalines <picalines0@gmail.com>
2024-08-31 14:53:58 +0300

    feat(mini-cursorword) add mini.cursorword

---
+++ nvim/lua/plugins/ui/mini-cursorword.lua
@@ -0,0 +1,35 @@
+return {
+	'echasnovski/mini.cursorword',
+
+	event = { 'BufReadPre', 'BufNewFile' },
+
+	opts = {
+		delay = 250,
+	},
+
+	config = function(_, opts)
+		local autocmd = require 'util.autocmd'
+		local hl = require 'util.highlight'
+
+		require('mini.cursorword').setup(opts)
+
+		hl.link('MiniCursorword', 'CursorLine')
+
+		local function disable_mini_cursorword(event)
+			vim.b[event.buf].minicursorword_disable = true
+		end
+
+		autocmd.on_filetype({
+			'help',
+			'lazy',
+			'neo-tree',
+			'neo-tree-popup',
+			'neotest-output',
+			'neotest-output-panel',
+			'neotest-summary',
+			'noice',
+		}, disable_mini_cursorword)
+
+		autocmd.on({ 'TermOpen', 'BufEnter' }, 'term://*', disable_mini_cursorword)
+	end,
+}
