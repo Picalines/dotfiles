c5d88d7ef856816f3d66606dda705d811cdfe7f0
picalines <picalines0@gmail.com>
2024-09-06 14:59:48 +0300

    feat(quickfix): dd to remove entry

--- nvim/lua/keymaps/ui/quickfix.lua
+++ nvim/lua/keymaps/ui/quickfix.lua
@@ -3,24 +3,23 @@ local autocmd = require 'util.autocmd'
 
 keymap.declare {
 	[{ 'n', silent = true }] = {
+		['<leader>q'] = { '<Cmd>copen<CR>', 'Open quickfix list' },
+
 		[']q'] = { '<Cmd>cnext<CR>zz', 'Go to next quickfix item' },
 		['[q'] = { '<Cmd>cprevious<CR>zz', 'Go to previous quickfix item' },
-		['<leader>q'] = { '<Cmd>copen<CR>', 'Open quickfix list' },
+
+		[']Q'] = { '<Cmd>cnewer<CR>', 'Go to newer quickfix list' },
+		['[Q'] = { '<Cmd>colder<CR>', 'Go to older quickfix list' },
 	},
 }
 
 autocmd.on_filetype('qf', function(event)
-	vim.bo[event.buf].modifiable = true
-
 	keymap.declare {
 		[{ 'n', remap = true, silent = true, buffer = event.buf }] = {
 			['q'] = { '<Cmd>cclose<CR>', 'Close quickfix list' },
 			['<leader>q'] = { '<Cmd>cclose<CR>', 'Close quickfix list' },
-			['n'] = { '<Cmd>cnewer<CR>', 'Go to newer quickfix list' },
-			['p'] = { '<Cmd>colder<CR>', 'Go to older quickfix list' },
 
-			['J'] = '<Cmd>cnext<CR>zz<C-w>w',
-			['K'] = '<Cmd>cprev<CR>zz<C-w>w',
+			['dd'] = { "<Cmd>call setqflist(filter(getqflist(), {idx -> idx != line('.') - 1}), 'r') <Bar> cc<CR>", 'Remove entry' },
 
 			['<leader>r'] = { '<Cmd>cdo s/// | update<C-Left><C-Left><Left><Left><Left>', 'Begin substitution' },
 		},
