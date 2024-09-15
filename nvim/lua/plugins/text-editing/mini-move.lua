71ee1391b98b0c81f122d1f85babb3b15a681d49
Picalines <picalines0@gmail.com>
2024-09-15 22:22:20 +0300

    fix(mini-move): change prefix to C-S

--- nvim/lua/plugins/text-editing/mini-move.lua
+++ nvim/lua/plugins/text-editing/mini-move.lua
@@ -4,20 +4,37 @@ return {
 	event = 'VeryLazy',
 
 	opts = {
-		mappings = {
-			left = '<M-h>',
-			right = '<M-l>',
-			down = '<M-j>',
-			up = '<M-k>',
-
-			line_left = '<M-h>',
-			line_right = '<M-l>',
-			line_down = '<M-j>',
-			line_up = '<M-k>',
-		},
+		mappings = {},
 
 		options = {
 			reindent_linewise = true,
 		},
 	},
+
+	config = function(_, opts)
+		local keymap = require 'util.keymap'
+		local mini_move = require 'mini.move'
+		local func = require 'util.func'
+
+		mini_move.setup(opts)
+
+		local move_line = mini_move.move_line
+		local move_selection = mini_move.move_selection
+
+		keymap.declare {
+			[{ 'n' }] = {
+				['<C-S-h>'] = { func.curry(move_line, 'left'), 'Unindent line' },
+				['<C-S-j>'] = { func.curry(move_line, 'down'), 'Move line down' },
+				['<C-S-k>'] = { func.curry(move_line, 'up'), 'Move line up' },
+				['<C-S-l>'] = { func.curry(move_line, 'right'), 'Indent line' },
+			},
+
+			[{ 'v' }] = {
+				['<C-S-h>'] = { func.curry(move_selection, 'left'), 'Move selection left' },
+				['<C-S-j>'] = { func.curry(move_selection, 'down'), 'Move selection down' },
+				['<C-S-k>'] = { func.curry(move_selection, 'up'), 'Move selection up' },
+				['<C-S-l>'] = { func.curry(move_selection, 'right'), 'Move selection right' },
+			},
+		}
+	end,
 }
