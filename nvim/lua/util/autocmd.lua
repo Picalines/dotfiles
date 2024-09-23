2529479d741d62fcb3bbd0ec50e5022beaf5ec87
Picalines <picalines0@gmail.com>
2024-09-23 16:49:06 +0300

    feat(autocmd): accept cmd in arg

--- nvim/lua/util/autocmd.lua
+++ nvim/lua/util/autocmd.lua
@@ -11,24 +11,46 @@ local on_group = vim.api.nvim_create_augroup('autocmd.on', { clear = true })
 
 ---@param event string | string[]
 ---@param pattern string | string[]
----@param callback fun(event: autocmd_event)
-function M.on(event, pattern, callback)
+---@param callback_or_cmd fun(event: autocmd_event) | string
+function M.on(event, pattern, callback_or_cmd)
+	local callback, cmd
+
+	if type(callback_or_cmd) == 'function' then
+		callback = callback_or_cmd
+		cmd = nil
+	else
+		callback = nil
+		cmd = callback_or_cmd
+	end
+
 	vim.api.nvim_create_autocmd(event, {
 		group = on_group,
 		pattern = pattern,
 		callback = callback,
+		command = cmd,
 	})
 end
 
 local on_user_group = vim.api.nvim_create_augroup('autocmd.on_user', { clear = true })
 
 ---@param event string | string[]
----@param callback fun(event: autocmd_event)
-function M.on_user(event, callback)
+---@param callback_or_cmd fun(event: autocmd_event) | string
+function M.on_user(event, callback_or_cmd)
+	local callback, cmd
+
+	if type(callback_or_cmd) == 'function' then
+		callback = callback_or_cmd
+		cmd = nil
+	else
+		callback = nil
+		cmd = callback_or_cmd
+	end
+
 	vim.api.nvim_create_autocmd('User', {
 		group = on_user_group,
 		pattern = event,
 		callback = callback,
+		command = cmd,
 	})
 end
 
