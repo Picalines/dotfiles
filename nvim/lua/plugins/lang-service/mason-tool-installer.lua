4b3b774c1976b1c5158c34de2253862f7ba7f7d2
Picalines <picalines0@gmail.com>
2024-10-09 00:53:09 +0300

    feat(mason, treesitter): auto install mason packages based on executables

---
+++ nvim/lua/plugins/lang-service/mason-tool-installer.lua
@@ -0,0 +1,70 @@
+return {
+	'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+	event = 'UiEnter',
+
+	opts = {
+		run_on_start = true,
+		auto_update = true,
+		debounce_hours = 6,
+
+		ensure_installed = {
+			'jsonls',
+			'lua_ls',
+			'stylua',
+			'vim-language-server',
+		},
+	},
+
+	config = function(_, base_opts)
+		local tbl = require 'util.table'
+		local array = require 'util.array'
+
+		local packages_by_executables = {
+			[{ 'npm' }] = {
+				'biome',
+				'graphql-language-service-cli',
+				'prettierd',
+				'tailwindcss-language-server',
+				'typescript-language-server',
+				{ 'eslint-lsp', version = '4.8.0' },
+			},
+
+			[{ 'python', 'python3' }] = {
+				'black',
+				'isort',
+				'pyright',
+			},
+
+			[{ 'dotnet' }] = {
+				'csharpier',
+			},
+
+			[{ 'docker' }] = {
+				'docker-compose-language-service',
+				'dockerfile-language-server',
+			},
+
+			[{ 'go' }] = {
+				'gopls',
+			},
+		}
+
+		local ensure_installed = base_opts.ensure_installed or {}
+
+		for executables, packages in pairs(packages_by_executables) do
+			local should_install = array.some(executables, function(exe)
+				return vim.fn.executable(exe) == 1
+			end)
+
+			if should_install then
+				ensure_installed = array.concat(ensure_installed, packages)
+			end
+		end
+
+		local opts = tbl.copy_deep(base_opts)
+		opts.ensure_installed = ensure_installed
+
+		require('mason-tool-installer').setup(opts)
+	end,
+}
