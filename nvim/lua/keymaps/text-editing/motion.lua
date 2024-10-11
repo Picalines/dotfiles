f3d557ce1bfd13ced452eb43537f12278a6a8bfe
Picalines <picalines0@gmail.com>
2024-10-11 12:12:37 +0300

    feat(keymaps): goto next error/warning

--- nvim/lua/keymaps/text-editing/motion.lua
+++ nvim/lua/keymaps/text-editing/motion.lua
@@ -1,6 +1,10 @@
 local keymap = require 'util.keymap'
 local func = require 'util.func'
 
+local goto_next = vim.diagnostic.goto_next
+local goto_prev = vim.diagnostic.goto_prev
+local severity = vim.diagnostic.severity
+
 keymap.declare {
 	[{ 'n', 'v' }] = {
 		['<Space>'] = '<Nop>',
@@ -25,7 +29,11 @@ keymap.declare {
 		-- nvim 0.11
 		-- ['[d'] = { func.curry(vim.diagnostic.jump, { count = -1, float = false }), 'Go to previous diagnostic' },
 		-- [']d'] = { func.curry(vim.diagnostic.jump, { count = 1, float = false }), 'Go to next diagnostic' },
-		[']d'] = { func.curry(vim.diagnostic.goto_next, { float = false }), 'Go to previous diagnostic' },
-		['[d'] = { func.curry(vim.diagnostic.goto_prev, { float = false }), 'Go to next diagnostic' },
+		[']d'] = { func.curry(goto_next, { float = false }), 'Go to next diagnostic' },
+		['[d'] = { func.curry(goto_prev, { float = false }), 'Go to previous diagnostic' },
+		[']e'] = { func.curry(goto_next, { float = false, severity = severity.ERROR }), 'Go to next error' },
+		['[e'] = { func.curry(goto_prev, { float = false, severity = severity.ERROR }), 'Go to previous error' },
+		[']w'] = { func.curry(goto_next, { float = false, severity = severity.WARN }), 'Go to next warning' },
+		['[w'] = { func.curry(goto_prev, { float = false, severity = severity.WARN }), 'Go to previous warning' },
 	},
 }
