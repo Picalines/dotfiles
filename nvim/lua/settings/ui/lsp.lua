d33577d333856545eedeb8b99e20f37aef5ebdbd
Picalines <picalines0@gmail.com>
2024-11-01 11:06:20 +0300

    feat(lsp): simpler diagnostics popup

--- nvim/lua/settings/ui/lsp.lua
+++ nvim/lua/settings/ui/lsp.lua
@@ -8,5 +8,34 @@ vim.diagnostic.config {
 			[vim.diagnostic.severity.INFO] = '',
 			[vim.diagnostic.severity.HINT] = '',
 		},
+
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+			[vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+			[vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+			[vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+		},
+	},
+
+	float = {
+		-- NOTE: format can't return the highlight, so we use prefix & suffix without it
+		format = func.const '',
+
+		prefix = function(report)
+			local signs = vim.diagnostic.config().signs or {}
+			local icon = signs.text[report.severity]
+			return icon .. ' ', signs.numhl[report.severity]
+		end,
+
+		suffix = function(report)
+			local hl = 'Normal'
+			local message = vim.trim(report.user_data.lsp.message)
+
+			if string.find(message, '\n') then
+				return string.format('%s\n^ (%s)', message, report.code), hl
+			end
+
+			return string.format('%s (%s)', message, report.code), hl
+		end,
 	},
 }
