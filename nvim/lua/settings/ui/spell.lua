6485690a2f98fe44f0914c0c41260c6c762cac57
Picalines <picalines0@gmail.com>
2024-12-13 17:02:00 +0300

    feat(spell): custom.spell

--- nvim/lua/settings/ui/spell.lua
+++ nvim/lua/settings/ui/spell.lua
@@ -1,4 +1,5 @@
 -- NOTE: netrw is needed. Without it, neovim doesn't show the download prompt at startup
 vim.opt.spelllang = { 'en_us', 'ru_yo' }
+vim.opt.spellfile = (vim.fn.stdpath 'config') .. '/spell/custom.utf-8.add'
 
 vim.opt.spelloptions = { 'camel' }
