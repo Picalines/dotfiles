acdd7a7bca8a1a0b3265f04ddcf374d261a43e0b
Picalines <picalines0@gmail.com>
2024-12-22 02:37:54 +0300

    feat(fugitive): toggle mapping

--- nvim/lua/plugins/ui/fugitive.lua
+++ nvim/lua/plugins/ui/fugitive.lua
@@ -1,3 +1,30 @@
+local keymap = require 'util.keymap'
+
+keymap.declare {
+	[{ 'n' }] = {
+		['<leader>v'] = '<Cmd>Git<CR>',
+	},
+}
+
 return {
 	'tpope/vim-fugitive',
+
+	cmd = { 'Git', 'G' },
+
+	config = function()
+		local autocmd = require 'util.autocmd'
+
+		local augroup = autocmd.group 'fugitive'
+
+		augroup:on('BufEnter', 'fugitive://*', function(event)
+			vim.api.nvim_set_option_value('buflisted', false, { buf = event.buf })
+
+			keymap.declare {
+				[{ 'n', buffer = event.buf, desc = 'Git: %s' }] = {
+					['<leader>v'] = { '<Cmd>wincmd c | wincmd p<CR>', 'close' },
+					['q'] = { '<Cmd>wincmd c | wincmd p<CR>', 'close' },
+				},
+			}
+		end)
+	end,
 }
