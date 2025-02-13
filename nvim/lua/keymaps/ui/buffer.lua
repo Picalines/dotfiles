031339c54230d7a5d76e9edd12f92d8e1656e50d
Picalines <picalines0@gmail.com>
2025-02-13 15:42:18 +0300

    feat(keymaps): ...
    It's like 5th iteration of buffer closing. And i think i finally get it
    
    The key is that whe :w command might fail, and i want to abort the
    mapping if it happends. Thats why i have double <Cmd><CR> here

--- nvim/lua/keymaps/ui/buffer.lua
+++ nvim/lua/keymaps/ui/buffer.lua
@@ -1,12 +1,37 @@
+local array = require 'util.array'
 local keymap = require 'util.keymap'
+
+local function number_of_buffers()
+	return #array.filter(vim.api.nvim_list_bufs(), function(bufnr)
+		return vim.api.nvim_get_option_value('buflisted', { buf = bufnr })
+	end)
+end
+
+local function add_safe_empty_buffer()
+	return number_of_buffers() <= 1 and '<Cmd>enew | b #<CR>' or ''
+end
 
 keymap.declare {
 	[{ 'n', silent = true, desc = 'Buffer: %s' }] = {
 		['<C-b>n'] = { '<Cmd>enew<CR>', 'new' },
 		['<C-b>r'] = { '<Cmd>e<CR>', 'reload' },
-		['<C-b>c'] = { '<Cmd>exe "silent w" | bd<CR>', 'close' },
-		['<C-b>C'] = { '<Cmd>bd!<CR>', 'close without saving' },
 		[']b'] = { '<Cmd>bn<CR>', 'next' },
 		['[b'] = { '<Cmd>bp<CR>', 'previous' },
+
+		['<C-b>c'] = {
+			desc = 'close',
+			expr = true,
+			function()
+				return add_safe_empty_buffer() .. '<Cmd>silent w!<CR><Cmd>bnext | bd #<CR>'
+			end,
+		},
+
+		['<C-b>C'] = {
+			desc = 'close without saving',
+			expr = true,
+			function()
+				return add_safe_empty_buffer() .. '<Cmd>bprev | bd! #<CR>'
+			end,
+		},
 	},
 }
