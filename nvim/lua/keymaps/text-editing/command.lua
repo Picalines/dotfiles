3859a941062f480bc17980febf943d5f72d9af02
Picalines <picalines0@gmail.com>
2025-02-17 13:28:49 +0300

    feat(keymaps): <leader>s -> g/

--- nvim/lua/keymaps/text-editing/command.lua
+++ nvim/lua/keymaps/text-editing/command.lua
@@ -5,18 +5,20 @@ keymap.declare {
 		['<leader>w'] = { '<Cmd>silent w<CR>', 'Write file' },
 		['<leader>W'] = { '<Cmd>silent wa!<CR>', 'Write all' },
 
-		['<leader>s'] = { ':%s//g<Left><Left>', 'Substitute' },
-		['<leader>g'] = { ':%g/', 'Global command' },
+		['g/'] = { ':%s//g<Left><Left>', 'Substitute' },
+		['g:'] = { ':%g/', 'Global command' },
+
+		['g<CR>'] = { ':lua vim.ui.open([[<C-r>=expand("<cfile>")<CR>]])<CR>', 'Open in system app' },
 	},
 
 	[{ 'x' }] = {
-		['<leader>s'] = { ':s//g<Left><Left>', 'Substitute' },
-		['<leader>g'] = { ':g/', 'Global command' },
+		['g/'] = { ':s//g<Left><Left>', 'Substitute' },
+		['g:'] = { ':g/', 'Global command' },
 
 		['p'] = '"_dP',
 	},
 
-	[{ 'n', 'v' }] = {
+	[{ 'n', 'x' }] = {
 		['<leader>x'] = { '"_', 'Void register' },
 	},
 }
