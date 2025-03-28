69844427d3bccdf592c764b81b2d264b6f0d3f9a
Picalines <picalines0@gmail.com>
2025-03-29 00:21:12 +0300

    refactor: diagnostic keymaps for 0.11

--- nvim/lua/keymaps/text-editing/motion.lua
+++ nvim/lua/keymaps/text-editing/motion.lua
@@ -1,8 +1,6 @@
 local keymap = require 'util.keymap'
 local func = require 'util.func'
 
-local goto_next = vim.diagnostic.goto_next
-local goto_prev = vim.diagnostic.goto_prev
 local severity = vim.diagnostic.severity
 
 keymap.declare {
@@ -15,10 +13,6 @@ keymap.declare {
 		['n'] = 'nzzzv',
 		['N'] = 'Nzzzv',
 
-		-- col('.') == 1 || col('.') > match(getline('.'), '\\S') + 1 ? '^' : '0'
-		['H'] = '^',
-		['L'] = '$',
-
 		[{ expr = true }] = {
 			['k'] = "v:count == 0 ? 'gk' : 'k'",
 			['j'] = "v:count == 0 ? 'gj' : 'j'",
@@ -26,14 +20,11 @@ keymap.declare {
 	},
 
 	[{ 'n', silent = true }] = {
-		-- nvim 0.11
-		-- ['[d'] = { func.curry(vim.diagnostic.jump, { count = -1, float = false }), 'Go to previous diagnostic' },
-		-- [']d'] = { func.curry(vim.diagnostic.jump, { count = 1, float = false }), 'Go to next diagnostic' },
-		[']d'] = { func.curry(goto_next, { float = false }), 'Go to next diagnostic' },
-		['[d'] = { func.curry(goto_prev, { float = false }), 'Go to previous diagnostic' },
-		[']e'] = { func.curry(goto_next, { float = false, severity = severity.ERROR }), 'Go to next error' },
-		['[e'] = { func.curry(goto_prev, { float = false, severity = severity.ERROR }), 'Go to previous error' },
-		[']w'] = { func.curry(goto_next, { float = false, severity = severity.WARN }), 'Go to next warning' },
-		['[w'] = { func.curry(goto_prev, { float = false, severity = severity.WARN }), 'Go to previous warning' },
+		['[d'] = { func.curry(vim.diagnostic.jump, { count = -1, float = false }), 'Go to previous diagnostic' },
+		[']d'] = { func.curry(vim.diagnostic.jump, { count = 1, float = false }), 'Go to next diagnostic' },
+		['[e'] = { func.curry(vim.diagnostic.jump, { count = -1, float = false, severity = severity.ERROR }), 'Go to previous error' },
+		[']e'] = { func.curry(vim.diagnostic.jump, { count = 1, float = false, severity = severity.ERROR }), 'Go to next error' },
+		['[w'] = { func.curry(vim.diagnostic.jump, { count = -1, float = false, severity = severity.WARN }), 'Go to previous warning' },
+		[']w'] = { func.curry(vim.diagnostic.jump, { count = 1, float = false, severity = severity.WARN }), 'Go to next warning' },
 	},
 }
