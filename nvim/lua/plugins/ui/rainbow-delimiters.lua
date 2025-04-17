9a0404507e7fca60621bfee677ae87f90bbc0423
Picalines <picalines0@gmail.com>
2025-04-17 20:40:45 +0300

    feat(ui): rainbow-delimiters

---
+++ nvim/lua/plugins/ui/rainbow-delimiters.lua
@@ -0,0 +1,15 @@
+return {
+	'HiPhish/rainbow-delimiters.nvim',
+
+	main = 'rainbow-delimiters.setup',
+
+	opts = {
+		highlight = {
+			'RainbowDelimiterYellow',
+			'RainbowDelimiterBlue',
+			'RainbowDelimiterGreen',
+			'RainbowDelimiterViolet',
+			'RainbowDelimiterCyan',
+		},
+	},
+}
