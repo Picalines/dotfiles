47002a4c39b97cf5d4cdd6597f9b62ac4d1a2153
Picalines <picalines0@gmail.com>
2025-04-18 18:03:53 +0300

    fix(neovide): migrate neovide_opacity

--- nvim/lua/keymaps/ui/neovide/window.lua
+++ nvim/lua/keymaps/ui/neovide/window.lua
@@ -1,19 +1,19 @@
 local keymap = require 'util.keymap'
 local signal = require 'util.signal'
 
-local neovide_transparency = signal.new(1)
-signal.persist(neovide_transparency, 'neovide.transparency')
+local neovide_opacity = signal.new(1)
+signal.persist(neovide_opacity, 'neovide.opacity')
 signal.watch(function()
-	vim.g.neovide_transparency = neovide_transparency()
+	vim.g.neovide_opacity = neovide_opacity()
 	vim.g.neovide_window_blurred = true
 end)
 
-local function toggle_transparency()
-	neovide_transparency(neovide_transparency() == 1 and 0.75 or 1)
+local function toggle_opacity()
+	neovide_opacity(neovide_opacity() == 1 and 0.75 or 1)
 end
 
 keymap.declare {
 	[{ 'n', nowait = true }] = {
-		['<leader><leader>t'] = { toggle_transparency, 'Neovide: Toggle transparency' },
+		['<leader><leader>t'] = { toggle_opacity, 'Neovide: Toggle opacity' },
 	},
 }
