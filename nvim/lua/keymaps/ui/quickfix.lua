52b02f11393bdeeb80c97fe5db503e69aca0b3f0
Picalines <picalines0@gmail.com>
2025-05-06 00:16:53 +0300

    chore(keymaps): simpler quickfix maps

--- nvim/lua/keymaps/ui/quickfix.lua
+++ nvim/lua/keymaps/ui/quickfix.lua
@@ -2,14 +2,14 @@ local keymap = require 'util.keymap'
 local autocmd = require 'util.autocmd'
 
 keymap {
-	[{ 'n', silent = true }] = {
-		['<leader>q'] = { '<Cmd>copen<CR>', 'Open quickfix list' },
+	[{ 'n', silent = true, desc = 'Quickfix: %s' }] = {
+		['<leader>q'] = { '<Cmd>copen<CR>', 'open' },
 
-		[']q'] = { '<Cmd>cnext<CR>zz', 'Go to next quickfix item' },
-		['[q'] = { '<Cmd>cprevious<CR>zz', 'Go to previous quickfix item' },
+		[']q'] = { '<Cmd>cnext<CR>zz', 'next item' },
+		['[q'] = { '<Cmd>cprevious<CR>zz', 'prev item' },
 
-		[']Q'] = { '<Cmd>cnewer<CR>', 'Go to newer quickfix list' },
-		['[Q'] = { '<Cmd>colder<CR>', 'Go to older quickfix list' },
+		[']Q'] = { '<Cmd>cnewer<CR>', 'newer list' },
+		['[Q'] = { '<Cmd>colder<CR>', 'older list' },
 	},
 }
 
@@ -17,13 +17,9 @@ local augroup = autocmd.group 'quickfix'
 
 augroup:on('FileType', 'qf', function(event)
 	keymap {
-		[{ 'n', remap = true, silent = true, buffer = event.buf }] = {
-			['q'] = { '<Cmd>cclose<CR>', 'Close quickfix list' },
-			['<leader>q'] = { '<Cmd>cclose<CR>', 'Close quickfix list' },
-
-			['dd'] = { "<Cmd>call setqflist(filter(getqflist(), {idx -> idx != line('.') - 1}), 'r') <Bar> cc<CR>", 'Remove entry' },
-
-			['<leader>r'] = { '<Cmd>cdo s/// | update<C-Left><C-Left><Left><Left><Left>', 'Begin substitution' },
+		[{ 'n', remap = true, silent = true, buffer = event.buf, desc = 'Quickfix: %s' }] = {
+			['q'] = { '<Cmd>cclose<CR>', 'close' },
+			['<leader>q'] = { '<Cmd>cclose<CR>', 'close' },
 		},
 	}
 end)
