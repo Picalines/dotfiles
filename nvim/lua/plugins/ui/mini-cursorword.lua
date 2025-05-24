cfd9b8ef7b1ea00e41e65e6e18fd4fe65afc26d4
Picalines <picalines0@gmail.com>
2025-05-24 23:37:23 +0300

    fix(mini-cursorword): simpler exclude list

--- nvim/lua/plugins/ui/mini-cursorword.lua
+++ nvim/lua/plugins/ui/mini-cursorword.lua
@@ -11,27 +11,15 @@ return {
 		local autocmd = require 'util.autocmd'
 		local hl = require 'util.highlight'
 
-		require('mini.cursorword').setup(opts)
-
 		hl.link('MiniCursorword', 'CursorLine')
-
-		local function disable_mini_cursorword(event)
-			vim.api.nvim_buf_set_var(event.buf, 'minicursorword_disable', true)
-		end
 
 		local augroup = autocmd.group 'mini-cursorword'
 
-		augroup:on('FileType', {
-			'help',
-			'lazy',
-			'neo-tree',
-			'neo-tree-popup',
-			'neotest-output',
-			'neotest-output-panel',
-			'neotest-summary',
-			'noice',
-		}, disable_mini_cursorword)
+		augroup:on({ 'BufEnter', 'TermOpen' }, '*', function(event)
+			local buftype = vim.api.nvim_get_option_value('buftype', { buf = event.buf })
+			vim.api.nvim_buf_set_var(event.buf, 'minicursorword_disable', buftype ~= '')
+		end)
 
-		augroup:on({ 'BufEnter', 'TermOpen' }, 'term://*', disable_mini_cursorword)
+		require('mini.cursorword').setup(opts)
 	end,
 }
