5a2a2cb29e0b273ef7bf9f85be4c7ef55fe0b9f0
Picalines <picalines0@gmail.com>
2025-05-24 23:29:03 +0300

    chore(fugitive): autocmd to top

--- nvim/lua/plugins/ui/fugitive.lua
+++ nvim/lua/plugins/ui/fugitive.lua
@@ -1,3 +1,4 @@
+local autocmd = require 'util.autocmd'
 local keymap = require 'util.keymap'
 
 keymap {
@@ -10,36 +11,35 @@ keymap {
 	},
 }
 
+local augroup = autocmd.group 'fugitive'
+
+augroup:on('BufEnter', 'fugitive://*', function(event)
+	vim.api.nvim_set_option_value('buflisted', false, { buf = event.buf })
+	vim.api.nvim_set_option_value('number', false, { win = vim.fn.bufwinid(event.buf) })
+	vim.api.nvim_set_option_value('relativenumber', false, { win = vim.fn.bufwinid(event.buf) })
+
+	keymap {
+		[{ 'n', buffer = event.buf, desc = 'Git: %s' }] = {
+			['<leader>gg'] = { '<Cmd>wincmd c | wincmd p<CR>', 'close' },
+			['q'] = { '<Cmd>wincmd c | wincmd p<CR>', 'close' },
+		},
+	}
+end)
+
+augroup:on('FileType', { 'gitcommit', 'gitrebase' }, function(event)
+	keymap {
+		[{ 'n', buffer = event.buf, nowait = true, desc = 'Git: %s' }] = {
+			['<leader>w'] = { '<Cmd>w | bd<CR>', 'accept' },
+			['<CR>'] = { '<Cmd>w | bd<CR>', 'accept' },
+			['q'] = { 'ggdG<Cmd>w | bd<CR>', 'cancel' },
+		},
+	}
+end)
+
 return {
 	'tpope/vim-fugitive',
 
 	cmd = { 'Git', 'G' },
 
-	config = function()
-		local autocmd = require 'util.autocmd'
-
-		local augroup = autocmd.group 'fugitive'
-
-		augroup:on('BufEnter', 'fugitive://*', function(event)
-			vim.api.nvim_set_option_value('buflisted', false, { buf = event.buf })
-			vim.api.nvim_set_option_value('number', false, { win = vim.fn.bufwinid(event.buf) })
-			vim.api.nvim_set_option_value('relativenumber', false, { win = vim.fn.bufwinid(event.buf) })
-
-			keymap {
-				[{ 'n', buffer = event.buf, desc = 'Git: %s' }] = {
-					['<leader>gg'] = { '<Cmd>wincmd c | wincmd p<CR>', 'close' },
-					['q'] = { '<Cmd>wincmd c | wincmd p<CR>', 'close' },
-				},
-			}
-		end)
-
-		augroup:on('FileType', { 'gitcommit', 'gitrebase' }, function(event)
-			keymap {
-				[{ 'n', buffer = event.buf, nowait = true, desc = 'Git: %s' }] = {
-					['<leader>w'] = { '<Cmd>w | bd<CR>', 'accept' },
-					['q'] = { 'ggdG<Cmd>w | bd<CR>', 'cancel' },
-				},
-			}
-		end)
-	end,
+	ft = { 'gitcommit', 'gitrebase', 'fugitive' },
 }
