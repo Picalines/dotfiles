7d138a5e24499c092af18bb01c14d3262b8dc390
Picalines <picalines0@gmail.com>
2025-07-05 00:39:34 +0300

    feat(treesitter): migrate to main branch

--- nvim/lua/plugins/lang-service/treesitter.lua
+++ nvim/lua/plugins/lang-service/treesitter.lua
@@ -1,60 +1,17 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 
-	event = { 'BufReadPre', 'BufNewFile' },
-
+	lazy = false,
+	branch = 'main',
 	build = ':TSUpdate',
 
-	opts = {
-		auto_install = false,
-
-		highlight = { enable = true },
-
-		indent = { enable = true },
-
-		incremental_selection = { enable = false },
-
-		ensure_installed = {
-			'bash',
-			'c',
-			'c_sharp',
-			'cpp',
-			'css',
-			'dockerfile',
-			'gitignore',
-			'go',
-			'graphql',
-			'html',
-			'http',
-			'java',
-			'javascript',
-			'json',
-			'kotlin',
-			'lua',
-			'markdown',
-			'python',
-			'regex',
-			'rust',
-			'svelte',
-			'toml',
-			'tsx',
-			'typescript',
-			'vim',
-			'vimdoc',
-			'yaml',
-		},
-	},
-
-	config = function(_, opts)
-		require('nvim-treesitter.configs').setup(opts)
-	end,
-
 	dependencies = {
-		'nvim-treesitter/nvim-treesitter-textobjects',
-
+		{
+			'nvim-treesitter/nvim-treesitter-textobjects',
+			branch = 'main',
+		},
 		{
 			'windwp/nvim-ts-autotag',
-
 			opts = {
 				opts = {
 					enable_close = true,
@@ -63,5 +20,35 @@ return {
 				},
 			},
 		},
+		{
+			'JoosepAlviste/nvim-ts-context-commentstring',
+			opts = { enable_autocmd = false },
+			init = function()
+				local get_option = vim.filetype.get_option
+				---@diagnostic disable-next-line: duplicate-set-field
+				vim.filetype.get_option = function(filetype, option)
+					return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
+				end
+			end,
+		},
 	},
+
+	init = function()
+		local array = require 'util.array'
+		local autocmd = require 'util.autocmd'
+		local nvim_treesitter = require 'nvim-treesitter'
+
+		local augroup = autocmd.group 'nvim-treesitter'
+
+		local available_parsers = nvim_treesitter.get_available()
+
+		augroup:on('FileType', '*', function(event)
+			local language = vim.treesitter.language.get_lang(event.match)
+			if language and array.contains(available_parsers, language) then
+				nvim_treesitter.install(language):await(function()
+					vim.treesitter.start(event.buf, language)
+				end)
+			end
+		end)
+	end,
 }
