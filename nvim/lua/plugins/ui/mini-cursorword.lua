a7489c0739c4404433f025b001fa914bfbe5ea03
Picalines <picalines0@gmail.com>
2025-07-04 19:31:20 +0300

    chore(mini-cursorword): init

--- nvim/lua/plugins/ui/mini-cursorword.lua
+++ nvim/lua/plugins/ui/mini-cursorword.lua
@@ -1,24 +1,19 @@
 return {
 	'echasnovski/mini.cursorword',
 
-	event = { 'BufReadPre', 'BufNewFile' },
+	event = 'VeryLazy',
 
 	opts = {
 		delay = 250,
 	},
 
-	config = function(_, opts)
+	init = function()
 		local autocmd = require 'util.autocmd'
-		local hl = require 'util.highlight'
-
-		hl.link('MiniCursorword', 'CursorLine')
 
 		local augroup = autocmd.group 'mini-cursorword'
 
 		augroup:on({ 'BufEnter', 'TermOpen' }, '*', function(event)
 			vim.b[event.buf].minicursorword_disable = vim.bo[event.buf].buftype ~= ''
 		end)
-
-		require('mini.cursorword').setup(opts)
 	end,
 }
