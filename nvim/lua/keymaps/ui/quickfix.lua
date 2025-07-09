8b989636b8ef23c2365b257365630ce281a47c0f
Picalines <picalines0@gmail.com>
2025-07-09 11:31:51 +0300

    fix(quickfix): bottom pane, wincmd p

--- nvim/lua/keymaps/ui/quickfix.lua
+++ nvim/lua/keymaps/ui/quickfix.lua
@@ -3,7 +3,7 @@ local keymap = require 'util.keymap'
 
 keymap {
 	[{ 'n', silent = true, desc = 'Quickfix: %s' }] = {
-		['<leader>q'] = { '<Cmd>copen<CR>', 'open' },
+		['<leader>q'] = { '<Cmd>botright copen<CR>', 'open' },
 
 		[']q'] = { '<Cmd>cnext<CR>zz', 'next item' },
 		['[q'] = { '<Cmd>cprevious<CR>zz', 'prev item' },
@@ -22,8 +22,8 @@ augroup:on('FileType', 'qf', function(event)
 
 	keymap {
 		[{ 'n', remap = true, silent = true, buffer = event.buf, desc = 'Quickfix: %s' }] = {
-			['q'] = { '<Cmd>cclose<CR>', 'close' },
-			['<leader>q'] = { '<Cmd>cclose<CR>', 'close' },
+			['q'] = { '<Cmd>cclose | wincmd p<CR>', 'close' },
+			['<leader>q'] = { '<Cmd>cclose | wincmd p<CR>', 'close' },
 		},
 	}
 end)
