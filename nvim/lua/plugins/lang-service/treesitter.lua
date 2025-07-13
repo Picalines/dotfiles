291457e0f53a94d27c5bd60812202f2d90fb2005
Picalines <picalines0@gmail.com>
2025-07-13 14:01:26 +0300

    feat(treesitter): install by composite filetypes and oldfiles

--- nvim/lua/plugins/lang-service/treesitter.lua
+++ nvim/lua/plugins/lang-service/treesitter.lua
@@ -39,15 +39,77 @@ return {
 
 		local augroup = autocmd.group 'nvim-treesitter'
 
-		local available_parsers = nvim_treesitter.get_available()
+		augroup:on('UIEnter', '*', function()
+			local always_installed_parsers = {
+				'editorconfig',
+				'git_config',
+				'git_rebase',
+				'gitattributes',
+				'gitcommit',
+				'gitignore',
+				'json',
+				'json5',
+				'jsonc',
+				'lua',
+				'luadoc',
+				'markdown',
+				'toml',
+				'vim',
+				'vimdoc',
+				'xml',
+				'yaml',
+			}
+
+			vim.iter(always_installed_parsers):each(nvim_treesitter.install)
+		end)
+
+		local parsers_by_filetype = vim
+			.iter({
+				[{ 'bash', 'zsh' }] = { 'bash' },
+				[{ 'c', 'cpp', 'cs' }] = { 'c', 'cpp', 'c_sharp' },
+				[{ 'css' }] = { 'css' },
+				[{ 'dockerfile' }] = { 'dockerfile' },
+				[{ 'go' }] = { 'go', 'templ' },
+				[{ 'graphql' }] = { 'graphql' },
+				[{ 'html' }] = { 'html' },
+				[{ 'http' }] = { 'http' },
+				[{ 'javascript', 'typescript' }] = { 'javascript', 'typescript', 'jsdoc', 'css' },
+				[{ 'javascriptreact', 'typescriptreact' }] = { 'jsx', 'tsx', 'graphql', 'css' },
+				[{ 'python' }] = { 'python' },
+				[{ 'svelte' }] = { 'svelte' },
+				[{ 'vue' }] = { 'vue' },
+			})
+			:fold({}, function(acc, filetypes, parsers)
+				for _, filetype in ipairs(filetypes) do
+					acc[filetype] = parsers
+				end
+				return acc
+			end)
+
+		local function install_parsers(filetypes)
+			vim
+				.iter(filetypes)
+				:map(function(filetype)
+					return parsers_by_filetype[filetype]
+				end)
+				:flatten()
+				:each(nvim_treesitter.install)
+		end
+
+		augroup:on('UIEnter', '*', function()
+			local filetypes = vim.fn.uniq(vim
+				.iter(vim.v.oldfiles)
+				:map(function(oldfile)
+					local filetype = vim.filetype.match { filename = oldfile }
+					return filetype
+				end)
+				:totable())
+
+			install_parsers(filetypes)
+		end)
 
 		augroup:on('FileType', '*', function(event)
-			local language = vim.treesitter.language.get_lang(event.match)
-			if language and vim.list_contains(available_parsers, language) then
-				nvim_treesitter.install(language):await(function()
-					vim.treesitter.start(event.buf, language)
-				end)
-			end
+			install_parsers { event.match }
 		end)
 	end,
 }
