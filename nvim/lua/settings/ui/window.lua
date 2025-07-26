f987d7dc7523b271bd73c734d19ce7598543a534
Picalines <picalines0@gmail.com>
2025-07-26 17:13:54 +0300

    chore(settings): always show tabline

--- nvim/lua/settings/ui/window.lua
+++ nvim/lua/settings/ui/window.lua
@@ -1,6 +1,5 @@
 local autocmd = require 'util.autocmd'
 local keymap = require 'util.keymap'
-local options = require 'util.options'
 
 local augroup = autocmd.group 'window'
 
@@ -15,6 +14,7 @@ vim.go.scrolloff = 8
 vim.go.sidescrolloff = 16
 
 vim.go.laststatus = 3 -- global statusline
+vim.go.showtabline = 2
 
 vim.go.number = true
 vim.go.relativenumber = true
@@ -24,18 +24,6 @@ vim.go.fillchars = 'eob: '
 
 vim.go.list = true
 vim.go.listchars = 'tab:   ,trail:,extends:,precedes:,nbsp:'
-
-augroup:on({ 'TabNew', 'WinNew', 'WinEnter', 'BufWinEnter', 'TermOpen' }, '*', function()
-	local tab_winids = vim.api.nvim_tabpage_list_wins(0)
-
-	local has_normal_bufs = vim.iter(tab_winids):any(function(winid)
-		local _, bo = options.winlocal(winid)
-		local config = vim.api.nvim_win_get_config(winid)
-		return bo.buftype == '' and config.relative == ''
-	end)
-
-	vim.o.showtabline = has_normal_bufs and 2 or 1 -- always OR only when #tabpages > 1
-end)
 
 augroup:on('TextYankPost', '*', function()
 	vim.highlight.on_yank()
