2e9f1e6f47019a37a03c38dc99498067794d9345
Picalines <picalines0@gmail.com>
2025-08-06 23:43:00 +0300

    feat: replace fugitive with neogit

---
+++ nvim/lua/plugins/ui/neogit.lua
@@ -0,0 +1,44 @@
+return {
+	'NeogitOrg/neogit',
+
+	cmd = 'Neogit',
+
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+	},
+
+	init = function()
+		local keymap = require 'util.keymap'
+
+		keymap {
+			[{ 'n', desc = 'Git: %s' }] = {
+				['<leader>gg'] = { '<Cmd>Neogit<CR>', 'status' },
+			},
+		}
+	end,
+
+	---@module 'neogit'
+	---@type NeogitConfig
+	opts = {
+		disable_hint = true,
+
+		commit_editor = {
+			kind = 'tab',
+			staged_diff_split_kind = 'auto',
+		},
+
+		mappings = {
+			status = {
+				['='] = 'Toggle',
+			},
+			commit_editor = {
+				['<cr>'] = 'Submit',
+			},
+			rebase_editor = {
+				['<cr>'] = 'Submit',
+				['K'] = 'MoveUp',
+				['J'] = 'MoveDown',
+			},
+		},
+	},
+}
