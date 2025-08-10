3dbb537a720aaad497e6de3d8b7d400ac986a31e
Picalines <picalines0@gmail.com>
2025-08-10 23:44:17 +0300

    chore(snacks): organize

--- nvim/lua/plugins/ui/snacks.lua
+++ nvim/lua/plugins/ui/snacks.lua
@@ -5,20 +5,21 @@ return {
 		local keymap = require 'util.keymap'
 
 		keymap {
+			[{ 'n', desc = 'LSP: %s' }] = {
+				['gD'] = { '<Cmd>lua Snacks.picker.lsp_definitions()<CR>', 'definitions' },
+				['gR'] = { '<Cmd>lua Snacks.picker.lsp_references()<CR>', 'references' },
+				['gI'] = { '<Cmd>lua Snacks.picker.lsp_implementations()<CR>', 'implementations' },
+				['gT'] = { '<Cmd>lua Snacks.picker.lsp_type_definitions()<CR>', 'type definitions' },
+			},
+
 			[{ 'n', desc = 'Buffer: %s' }] = {
+				['<leader>bb'] = { '<Cmd>lua Snacks.picker.buffers()<CR>', 'find' },
 				['<leader>bd'] = { '<Cmd>lua Snacks.bufdelete()<CR>', 'delete' },
 				['<leader>bD'] = { '<Cmd>lua Snacks.bufdelete.all()<CR>', 'delete all' },
 				['<leader>bo'] = { '<Cmd>lua Snacks.bufdelete.other()<CR>', 'delete other' },
 			},
 
 			[{ 'n', desc = 'Find: %s' }] = {
-				['gD'] = { '<Cmd>lua Snacks.picker.lsp_definitions()<CR>', 'lsp definitions' },
-				['gR'] = { '<Cmd>lua Snacks.picker.lsp_references()<CR>', 'lsp references' },
-				['gI'] = { '<Cmd>lua Snacks.picker.lsp_implementations()<CR>', 'lsp implementations' },
-				['gT'] = { '<Cmd>lua Snacks.picker.lsp_type_definitions()<CR>', 'lsp type definitions' },
-
-				['<leader>bb'] = { '<Cmd>lua Snacks.picker.buffers()<CR>', 'buffer' },
-
 				['<leader>ff'] = { '<Cmd>lua Snacks.picker.files()<CR>', 'files' },
 				['<leader>fo'] = { '<Cmd>lua Snacks.picker.recent()<CR>', 'recent' },
 				['<leader>fb'] = { '<Cmd>lua Snacks.picker.buffers()<CR>', 'buffer' },
@@ -26,7 +27,9 @@ return {
 				['<leader>fh'] = { '<Cmd>lua Snacks.picker.help()<CR>', 'help' },
 				['<leader>fr'] = { '<Cmd>lua Snacks.picker.resume()<CR>', 'resume' },
 				['<leader>ft'] = { ':set filetype=', 'filetype' },
+			},
 
+			[{ 'n', desc = 'UI: %s' }] = {
 				['<leader>uC'] = { '<Cmd>lua Snacks.picker.colorschemes()<CR>', 'colorscheme' },
 			},
 		}
