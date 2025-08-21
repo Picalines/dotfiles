041057caf6b1ba2420658f02606b4d9f4ef49345
Picalines <picalines0@gmail.com>
2025-08-21 12:50:48 +0300

    feat(settings): guicursor

--- nvim/lua/settings/ui/guicursor.lua
+++ nvim/lua/settings/ui/guicursor.lua
@@ -1,14 +1,7 @@
-local app = require 'util.app'
-
-local guicursor = {
+vim.opt_global.guicursor = {
 	'n-v-c:block',
 	'i-ci-ve:ver25',
 	'r-cr:hor20',
 	'o:hor25',
+	'a:blinkwait500-blinkoff500-blinkon500-Cursor/lCursor',
 }
-
-if app.client() ~= 'terminal' then
-	table.insert(guicursor, 'a:blinkwait500-blinkoff500-blinkon500-Cursor/lCursor')
-end
-
-vim.go.guicursor = table.concat(guicursor, ',')
