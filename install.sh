8ab503db00bd1062fc98a12d729c4704d0a00284
picalines <picalines0@gmail.com>
2025-09-27 16:52:44 +0300

    feat(install): user powershell on windows

--- install.sh
+++ install.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
 
-mkdir -p ~/.config
+set -e
 
 command-exists() {
   command -v "$1" &> /dev/null
 }
 
 symlink-from-to() {
-    mkdir -p "$(dirname $2)"
-    ln -snf "$PWD/$1" "$2"
+    echo $1 $2
+    if [[ -n $WINDIR ]]; then
+        powershell "New-Item -Type SymbolicLink -Path $2 -Value $1 -Force"
+    else
+        mkdir -p "$(dirname $2)"
+        ln -snf "$PWD/$1" "$2"
+    fi
 }
 
-symlink-from-to ./.bashrc ~/.bashrc
-symlink-from-to ./.zshrc ~/.zshrc
-symlink-from-to ./.gitconfig ~/.gitconfig
-symlink-from-to ./.gitignore.global ~/.gitignore
+symlink-from-to ".bashrc" "~/.bashrc"
+symlink-from-to ".gitconfig" "~/.gitconfig"
+symlink-from-to ".gitignore.global" "~/.gitignore"
 
-if command-exists nvim
-then
-    symlink-from-to ./nvim ~/.config/nvim
-fi
-
-if command-exists aerospace
-then
-    symlink-from-to ./aerospace.toml ~/.config/aerospace/aerospace.toml
-fi
-
-if command-exists alacritty
-then
-    symlink-from-to ./alacritty.toml ~/.config/alacritty/alacritty.toml
-fi
-
-if command-exists ghostty
-then
-    symlink-from-to ./ghostty.config ~/.config/ghostty/config
-fi
-
-if command-exists neovide
-then
-    symlink-from-to ./neovide.toml ~/.config/neovide/config.toml
-fi
-
-if command-exists starship
-then
-    symlink-from-to ./starship.toml ~/.config/starship.toml
-fi
+command-exists zsh && symlink-from-to ".zshrc" "~/.zshrc"
+command-exists nvim && symlink-from-to "nvim" "~/.config/nvim"
+command-exists aerospace && symlink-from-to "aerospace.toml" "~/.config/aerospace/aerospace.toml"
+command-exists alacritty && symlink-from-to "alacritty.toml" "~/.config/alacritty/alacritty.toml"
+command-exists ghostty && symlink-from-to "ghostty.config" "~/.config/ghostty/config"
+command-exists neovide && symlink-from-to "neovide.toml" "~/.config/neovide/config.toml"
+command-exists starship && symlink-from-to "starship.toml" "~/.config/starship.toml"
