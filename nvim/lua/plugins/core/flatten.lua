f1f6ebea3c34193ee7f7528d837a526178ef44c2
picalines <picalines0@gmail.com>
2025-10-11 01:25:14 +0300

    feat(flatten): post_open

--- nvim/lua/plugins/core/flatten.lua
+++ nvim/lua/plugins/core/flatten.lua
@@ -9,5 +9,33 @@ return {
 			open = 'smart',
 			focus = 'last',
 		},
+
+		hooks = {
+			post_open = function(args)
+				local autocmd = require 'util.autocmd'
+				local keymap = require 'util.keymap'
+
+				vim.api.nvim_set_current_win(args.winnr)
+
+				if args.is_blocking then
+					autocmd.buffer(args.bufnr):on(
+						'BufWritePost',
+						vim.schedule_wrap(function()
+							require('snacks').bufdelete.delete(args.bufnr)
+						end)
+					)
+
+					keymap {
+						[{ 'n', buffer = args.bufnr }] = {
+							['<CR>'] = { '<Cmd>w<CR>' },
+						},
+					}
+				end
+			end,
+
+			block_end = vim.schedule_wrap(function()
+				vim.cmd.wincmd 'p'
+			end),
+		},
 	},
 }
