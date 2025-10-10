ec5cd13b06c4a02ac5d0c5f4cbc4806806162c5e
picalines <picalines0@gmail.com>
2025-10-11 00:58:04 +0300

    feat(codecompanion): bring back localleader, use tab

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,7 @@
 -- configuration
 
 vim.g.mapleader = ' '
+vim.g.maplocalleader = vim.keycode '<Tab>'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
