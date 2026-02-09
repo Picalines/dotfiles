48c8777409d4fb51557b3a811154697da178cd38
picalines <picalines0@gmail.com>
2026-02-09 22:37:42 +0300

    fix(guess-indent): don't expandtab in tabs

--- nvim/lua/plugins/edit/guess-indent.lua
+++ nvim/lua/plugins/edit/guess-indent.lua
@@ -1,12 +1,10 @@
 return {
 	'nmac427/guess-indent.nvim',
 
-	priority = 1000,
-
 	opts = {
 		auto_cmd = true,
 		on_tab_options = {
-			expandtab = true,
+			expandtab = false,
 			tabstop = 4,
 			softtabstop = 4,
 			shiftwidth = 4,
