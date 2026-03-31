8142a212e13dad1a2ba5dc21505ac1cd326c496f
picalines <picalines0@gmail.com>
2026-03-31 04:15:57 +0300

    chore(queries): format

--- nvim/queries/jsx/textobjects.scm
+++ nvim/queries/jsx/textobjects.scm
@@ -1,15 +1,17 @@
-;; extends
+; extends
 
-((jsx_element
+(jsx_element
   open_tag: _
   _+ @tag.inner
-  close_tag: _) @tag.outer)
+  close_tag: _) @tag.outer
 
 (jsx_element
-  open_tag: (_ name: _ @tag.name))
+  open_tag: (_
+    name: _ @tag.name))
 
 (jsx_element
-  close_tag: (_ name: _ @tag.name))
+  close_tag: (_
+    name: _ @tag.name))
 
-((jsx_self_closing_element
-   name: _ @tag.name) @tag.outer)
+(jsx_self_closing_element
+  name: _ @tag.name) @tag.outer
