87a356dd2f0d4bacc6f64e6feb7493e2704e8cd2
picalines <picalines0@gmail.com>
2026-04-13 22:26:27 +0300

    fix(spell): filetype autocmd

--- nvim/lua/settings/spell.lua
+++ nvim/lua/settings/spell.lua
@@ -1,17 +1,17 @@
 local autocmd = require 'util.autocmd'
-local options = require 'util.options'
 
 -- NOTE: netrw is needed. Without it, neovim doesn't show the download prompt at startup
+vim.opt.spell = false
 vim.opt.spelllang = { 'en_us', 'ru_yo' }
 vim.opt.spellfile = (vim.fn.stdpath 'config') .. '/spell/words.utf-8.add'
 vim.opt.spelloptions = { 'camel' }
 
 local augroup = autocmd.group 'spell'
 
-augroup:on('BufWinEnter', '*', function(event)
-	local bo, wo = options.buflocal(event.buf)
-	wo.spell = bo.buftype == ''
+augroup:on('FileType', '*', function()
+	vim.wo.spell = vim.bo.buftype == ''
 end)
 
+augroup:on('FileType', 'snippets', 'set nospell')
+
 augroup:on('TermOpen', '*', 'set nospell')
-augroup:on('BufWinEnter', '*.snippets', 'set nospell')
