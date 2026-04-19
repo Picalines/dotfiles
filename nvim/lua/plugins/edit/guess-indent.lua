f2bf7ba0fd33495e4e6358149656e06a063db29b
picalines <picalines0@gmail.com>
2026-04-19 14:37:06 +0300

    feat(guess-indent): mapping

--- nvim/lua/plugins/edit/guess-indent.lua
+++ nvim/lua/plugins/edit/guess-indent.lua
@@ -1,6 +1,17 @@
 return {
 	'nmac427/guess-indent.nvim',
 
+	init = function()
+		local keymap = require 'mappet'
+		local map = keymap.map
+
+		local keys = keymap.group 'guess-indent'
+
+		keys { 'n' } {
+			map('<LocalLeader>i', 'detect indent') '<Cmd>GuessIndent<CR>',
+		}
+	end,
+
 	opts = {
 		auto_cmd = true,
 		on_tab_options = {
