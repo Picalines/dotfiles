73e7236d48647df8e3b44df0a7e36b5e2329f70e
picalines <picalines0@gmail.com>
2026-08-04 00:00:04 +0300

    feat(pwsh): bring my common aliases
    Probably will look into mise aliases to keep them
    cross-{shell,platform}, but that's for another time

--- shell/pwsh/profile.ps1
+++ shell/pwsh/profile.ps1
@@ -1,3 +1,33 @@
-Get-Command mise *> $null && (mise activate pwsh | Out-String | Invoke-Expression)
+function Command-Exists([string]$Command) {
+	Get-Command $Command *> $null
+	return $?
+}
 
-Get-Command starship *> $null && (starship init powershell | Out-String | Invoke-Expression)
+if (Command-Exists scoop -and Command-Exists git) { Set-Alias bash "$(scoop prefix git)/bin/bash.exe" }
+
+if (Command-Exists mise) { mise activate pwsh | Out-String | Invoke-Expression }
+
+if (Command-Exists starship) { starship init powershell | Out-String | Invoke-Expression }
+
+if (Command-Exists zoxide) { zoxide init powershell | Out-String | Invoke-Expression }
+
+if (Command-Exists yazi) {
+	function f {
+		$tmp = (New-TemporaryFile).FullName
+		yazi.exe @args --cwd-file="$tmp"
+		$cwd = Get-Content -Path $tmp -Encoding UTF8
+		if ($cwd -and $cwd -ne $PWD.Path -and (Test-Path -LiteralPath $cwd -PathType Container)) {
+			Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+		}
+		Remove-Item -Path $tmp
+	}
+}
+
+Set-Alias e nvim
+
+Set-Alias g git
+Set-Alias gg lazygit
+
+Set-Alias m mise
+function mr { mise run @args }
+function me { mise exec @args }
